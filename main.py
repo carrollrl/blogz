8ce4b732ad97f61f99a801dc087b80eeb2f49f89
Robert Carroll <carroll.rl@gmail.com>
2018-05-04 08:51:40 -0500

    5-4-18, lots of errors

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from hashutils import make_pw_hash, check_pw_hash
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:launchcode@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secretkey = 'y337kGcys&zP3B'
 
 
 class Blog(db.Model):
@@ -16,36 +17,61 @@ class Blog(db.Model):
     body = db.Column(db.String(500))
     owner = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body, owner):
+    def __init__(self, title, body, owner_id):
         self.title = title
         self.body = body
-        self.owner = owner
+        self.owner_id = owner_id
 
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
-    pw_hash = db.Column(db.String(200))
-    blogs = db.relationship('Blog', backref='owner', lazy='joined')
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner_id', lazy='joined')
 
-    def __init__(self, username, pw_hash):
+    def __init__(self, username, password):
         self.username = username
-        self.pw_hash = make_pw_hash(password)
+        self.password = password
 
 
 @app.before_request
 def require_login():
     allowed_routes = ['login', 'blog', 'index', 'signup']
     if request.endpoint not in allowed_routes and 'username' not in session:
-        return render_template('login.html')
+        return redirect('/login')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user_variable = User.query.filter_by(username=username)
+        if user_variable and user_variable.password == password:
+            session['username'] = username
+            flash('Logged in')
+            return redirect('/newpost')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+            return redirect('/login')
+
+    return render_template('login.html')
+
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    del session['username']
+    return redirect('/blog')
+
 
 
 @app.route('/index', methods=['POST', 'GET'])
 def index():
     username_list = User.query.all()
-    return username_list
-
+    return render_template('index.html', username_list=username_list) 
+    
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
@@ -80,38 +106,19 @@ def signup():
         # validation
         if not username_error and not password_error and not verify_error:
             if not existing_user:
-                new_user = User(username, pw_hash)
+                new_user = User(username, password)
                 db.session.add(new_user)
                 db.session.commit()
                 session['username'] = username
                 return redirect('/newpost?username={0}'.format(new_user))
             else:
-                return render_template('signup.html', username=username, password=password, verify=verify, username_error=username_error, password_error=password_error, verify_error=verify_error)
+                return render_template('signup.html', username='', password='', verify='', username_error=username_error, password_error=password_error, verify_error=verify_error)
                 
         else:
             return render_template('signup.html', username_error=username_error, password_error=password_error, verify_error=verify_error, username=username, password='', verify='')
 
+    return render_template('signup.html')
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username and check_pw_hash(password, username.pw_hash):
-            session['username'] = username
-            flash('Logged in')
-            return redirect('/newpost')
-        else:
-            flash('User password incorrect, or user does not exist', 'error')
-            return redirect('/login')
-
-    return render_template
-    
-
-@app.route('/logout', methods=['POST'])
-def logout():
-    del session['username']
-    return redirect('/blog')
 
 
 @app.route('/blog', methods=['POST', 'GET'])
